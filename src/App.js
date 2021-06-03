cfa927e999c0788c274797b1d0ee0f88b1d55b6c
soykot2910 <ashrafulislam2910@gmail.com>
2021-06-03 11:17:17 +0600

    design done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import data from "./data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    image:
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    name: "sami",
+  };
+  render() {
+    return (
+      <div className="card">
+        <div className="celebration-card">
+          <img className="birthday-img" src="./images/birthday.jpg" alt="" />
+          <div className="birthday-info">
+            {/* <img
+              src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+              alt=""
+            /> */}
+            <img src={this.state.image} alt="no" />
+            <h2 className="wish">
+              It's {this.state.name}'s <br /> birthday today
+            </h2>
+          </div>
+          <div className="msg">
+            <div className="btn-msg">Congratulate</div>
+          </div>
+        </div>
+        <h1>This Month</h1>
+        {data.map(({ name, image, birthday }) => {
+          return (
+            <div className="list">
+              <img src={image} alt="" />
+              <div className="info">
+                <h3 className="name">{name}</h3>
+                <p className="birthdate">{birthday}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    id: 1,
+    name: "Bertie Yates",
+    birthday: "23 January 1990",
+    age: 28,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+  },
+  {
+    id: 2,
+    name: "Hester Hogan",
+    birthday: "23 January 1990",
+    age: 32,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
+  },
+  {
+    id: 3,
+    name: "Larry Little",
+    birthday: "23 January 1990",
+    age: 36,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+  {
+    id: 4,
+    name: "Sean Walsh",
+    birthday: "23 January 1990",
+    age: 34,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    name: "Lola Gardner",
+    birthday: "23 January 1990",
+    age: 29,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+];
+
+export default data;
